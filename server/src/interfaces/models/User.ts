936cdb671e0e9461660540f83b67677e2b2c487f
Angel Angeles <daniel_1234@hotmail.es>
2022-08-01 20:23:13 -0400

    adding user interface schema

--- server/src/interfaces/models/User.ts
+++ server/src/interfaces/models/User.ts
@@ -5,40 +5,45 @@
  */
 
 import { Express } from 'express';
+import mongoose = require("mongoose");
 
 export interface IUser extends Express.User {
     id: string;
-    email?: string;
+    email: string;
     phoneNumber?: string;
     password?: string;
-    passwordResetToken?: string;
-    passwordResetExpires?: Date;
-
-    tokens?: Tokens[];
-    profile?: FederatedAuthProfiles;
-    pictures?: UserPictures[];
 
     fullname: string;
     gender: string;
     userName: string;
 }
 
-export interface Tokens {
-    id: string;
-    kind: string;
-    accessToken: string;
-    tokenSecret?: string;
-}
-
-export interface FederatedAuthProfiles {
-    id: string;
-    kind: string;
-    profileId: string;
-}
-
-export interface UserPictures {
-    id: string;
-    imageUrl: string;
-}
+export const UserSchema = new mongoose.Schema<IUser>(
+    {
+        email: {
+            type: String,
+            required: true
+        },
+        phoneNumber: {
+            type: String,
+            required: false
+        },
+        fullname: {
+            type: String,
+            required: false
+        },
+        gender: {
+            type: String,
+            required: false
+        },
+        userName: {
+            type: String,
+            required: false
+        }
+    },
+    {
+        timestamps: true,
+    }
+);
 
 export default IUser;
