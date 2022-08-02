9511dfa9ce3cbbf031a9f7839dd507c9b44d0df6
Angel Angeles <daniel_1234@hotmail.es>
2022-08-01 20:04:19 -0400

    adding graphql consts

--- server/src/providers/Locals.ts
+++ server/src/providers/Locals.ts
@@ -14,20 +14,22 @@ class Locals {
      */
     public static config(): any {
         const port = process.env.PORT || 3001;
-        const DATABASE_URL = process.env.DATABASE_URL || 'postgres://admin:admin@localhost:5432/db';
 
         const appSecret = process.env.APP_SECRET || 'secret_key';
         const apiPrefix = process.env.API_PREFIX || 'api';
 
+        const is_user_logged = 'isUserLogged';
+        const gq_email = 'email';
+
         //allow origin cors
         const url = 'http://localhost:3000';
-
 
         return {
             apiPrefix,
             appSecret,
             port,
-            dbUrl: DATABASE_URL,
+            is_user_logged,
+            gq_email,
             url
         }
     }
